57c4f9fb88249c08799a7205510680f2b43d2e14
Alan Li <alan.li@me.com>
2019-10-02 19:14:40 -0700

    Add a helper function for getting 256 integer type.

--- llvm/lib/IR/LLVMContextImpl.h
+++ llvm/lib/IR/LLVMContextImpl.h
@@ -1464,7 +1464,7 @@ public:
   Type VoidTy, LabelTy, HalfTy, BFloatTy, FloatTy, DoubleTy, MetadataTy,
       TokenTy;
   Type X86_FP80Ty, FP128Ty, PPC_FP128Ty, X86_MMXTy, X86_AMXTy;
-  IntegerType Int1Ty, Int8Ty, Int16Ty, Int32Ty, Int64Ty, Int128Ty;
+  IntegerType Int1Ty, Int8Ty, Int16Ty, Int32Ty, Int64Ty, Int128Ty, Int256Ty;
 
   std::unique_ptr<ConstantTokenNone> TheNoneToken;
 

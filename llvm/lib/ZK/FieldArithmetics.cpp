79fb930e54c5ba141ac5f68b214a880b6de18a15
root <mail.nikita.kaskov@gmail.com>
2023-01-18 23:04:28 +0000

    Builtin field and curve types renamed. #5

--- llvm/lib/ZK/FieldArithmetics.cpp
+++ llvm/lib/ZK/FieldArithmetics.cpp
@@ -1,11 +1,11 @@
-#include <nil/marshalling/algorithms/pack.hpp>
-#include <nil/crypto3/marshalling/algebra/types/field_element.hpp>
-
-#include <nil/crypto3/algebra/random_element.hpp>
 #include <nil/crypto3/algebra/curves/bls12.hpp>
-#include <nil/crypto3/algebra/curves/detail/marshalling.hpp>
+#include <nil/crypto3/algebra/curves/curve25519.hpp>
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+#include <nil/crypto3/algebra/curves/vesta.hpp>
 #include <nil/crypto3/algebra/fields/pallas/base_field.hpp>
-#include <nil/crypto3/algebra/fields/curve25519/base_field.hpp>
+
+#include <nil/crypto3/marshalling/algebra/types/field_element.hpp>
+#include <nil/marshalling/algorithms/pack.hpp>
 
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallString.h"
@@ -20,10 +20,14 @@ static FieldElem FieldBinOpImpl(FieldOperation Op, const FieldElem &LHS,
   // TODO(maksenov): avoid copying here
   std::vector<char> bytes(APIntData, APIntData + LHS.getNumWords() * 8);
   nil::marshalling::status_type status;
-  typename FieldType::value_type lhsVal = nil::marshalling::pack<nil::marshalling::option::little_endian>(bytes, status);
+  typename FieldType::value_type lhsVal =
+      nil::marshalling::pack<nil::marshalling::option::little_endian>(bytes,
+                                                                      status);
   APIntData = reinterpret_cast<char *>(RHS.getData());
   bytes = std::vector(APIntData, APIntData + RHS.getNumWords() * 8);
-  typename FieldType::value_type rhsVal = nil::marshalling::pack<nil::marshalling::option::little_endian>(bytes, status);
+  typename FieldType::value_type rhsVal =
+      nil::marshalling::pack<nil::marshalling::option::little_endian>(bytes,
+                                                                      status);
   typename FieldType::value_type op_res;
   switch (Op) {
   default:
@@ -41,32 +45,56 @@ static FieldElem FieldBinOpImpl(FieldOperation Op, const FieldElem &LHS,
     op_res = lhsVal / rhsVal;
     break;
   }
-  std::vector<char> res_bytes = nil::marshalling::pack<nil::marshalling::option::little_endian>(op_res, status);
+  std::vector<char> res_bytes =
+      nil::marshalling::pack<nil::marshalling::option::little_endian>(op_res,
+                                                                      status);
   int padding = res_bytes.size() % 8;
   for (int i = 0; i < padding; ++i)
     res_bytes.push_back(0);
-  APInt res = APInt(FieldType::number_bits, makeArrayRef(reinterpret_cast<const uint64_t *>(res_bytes.data()), res_bytes.size() / 8));
+  APInt res =
+      APInt(FieldType::number_bits,
+            makeArrayRef(reinterpret_cast<const uint64_t *>(res_bytes.data()),
+                         res_bytes.size() / 8));
   return FieldElem(LHS.getKind(), res);
 }
 
-
-FieldElem FieldBinOp(FieldOperation Op, const FieldElem &LHS, const FieldElem &RHS) {
-    assert(LHS.getKind() == RHS.getKind());
-    switch (LHS.getKind()) {
-    case GALOIS_FIELD_BLS12_381_BASE:
-        return FieldBinOpImpl<nil::crypto3::algebra::fields::bls12_base_field<381>>
-        (Op, LHS, RHS);
-    case GALOIS_FIELD_PALLAS_BASE: {
-        return FieldBinOpImpl<nil::crypto3::algebra::fields::pallas_base_field>
-            (Op, LHS, RHS);
-    case GALOIS_FIELD_CURVE_25519_BASE:
-        return FieldBinOpImpl<nil::crypto3::algebra::fields::curve25519_base_field>
-            (Op, LHS, RHS);
-      }
-    }
+FieldElem FieldBinOp(FieldOperation Op, const FieldElem &LHS,
+                     const FieldElem &RHS) {
+  assert(LHS.getKind() == RHS.getKind());
+  switch (LHS.getKind()) {
+  case GALOIS_FIELD_BLS12381_BASE:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::bls12<381>::base_field_type>(Op, LHS,
+                                                                    RHS);
+  case GALOIS_FIELD_BLS12381_SCALAR:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::bls12<381>::scalar_field_type>(Op, LHS,
+                                                                      RHS);
+  case GALOIS_FIELD_PALLAS_BASE:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::pallas::base_field_type>(Op, LHS, RHS);
+  case GALOIS_FIELD_PALLAS_SCALAR:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::pallas::scalar_field_type>(Op, LHS, RHS);
+  case GALOIS_FIELD_VESTA_BASE:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::vesta::base_field_type>(Op, LHS, RHS);
+  case GALOIS_FIELD_VESTA_SCALAR:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::vesta::scalar_field_type>(Op, LHS, RHS);
+  case GALOIS_FIELD_CURVE25519_BASE:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::curve25519::base_field_type>(Op, LHS,
+                                                                    RHS);
+  case GALOIS_FIELD_CURVE25519_SCALAR:
+    return FieldBinOpImpl<
+        nil::crypto3::algebra::curves::curve25519::scalar_field_type>(Op, LHS,
+                                                                      RHS);
+  }
 }
 
-bool FieldElemFromStr(GaloisFieldKind Kind, StringRef StrRef, FieldElem &Result) {
+bool FieldElemFromStr(GaloisFieldKind Kind, StringRef StrRef,
+                      FieldElem &Result) {
   int radix = 10;
   SmallString<10> Str(StrRef);
   if (Str[0] == '0') {
@@ -92,4 +120,4 @@ bool FieldElemFromStr(GaloisFieldKind Kind, StringRef StrRef, FieldElem &Result)
   Result = FieldElem(Kind, ParsedInt);
   return true;
 }
-}
+} // namespace llvm

97ed4458aeb59243322f84904e7841606ca8b730
Alexander Evgin <aleasims@gmail.com>
2023-02-24 13:27:58 +0000

    Hotfix parsing arch name

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -322,6 +322,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("arm64_32", aarch64_32)
     .Case("arm", arm)
     .Case("armeb", armeb)
+    .Case("assigner", assigner)
     .Case("avr", avr)
     .StartsWith("bpf", BPFArch)
     .Case("m68k", m68k)

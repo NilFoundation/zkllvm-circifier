23d0cf4008f5d320b1be962d55db26af16d82545
Mikhail Aksenov <maksenov@nil.foundation>
2023-03-21 18:43:07 +0200

    Reenable InstSimplifyPass

--- llvm/lib/Transforms/Scalar/InstSimplifyPass.cpp
+++ llvm/lib/Transforms/Scalar/InstSimplifyPass.cpp
@@ -128,8 +128,6 @@ FunctionPass *llvm::createInstSimplifyLegacyPass() {
 
 PreservedAnalyses InstSimplifyPass::run(Function &F,
                                         FunctionAnalysisManager &AM) {
-  // TODO(maksenov): temporarily disabled, fix it
-  return PreservedAnalyses::all();
   auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
   auto &TLI = AM.getResult<TargetLibraryAnalysis>(F);
   auto &AC = AM.getResult<AssumptionAnalysis>(F);

b26db00d38ab4b693354730566aa1b7ec74b7b1f
Mikhail Aksenov <maksenov@nil.foundation>
2023-04-17 11:24:58 +0300

    Add initial toolchain for assigner

---
+++ clang/lib/Driver/ToolChains/Assigner.h
@@ -0,0 +1,66 @@
+//===--- Assigner.h - Assigner ToolChain Implementations --*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ASSIGNER_H
+#define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ASSIGNER_H
+
+#include "Gnu.h"
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/ToolChain.h"
+
+namespace clang {
+namespace driver {
+namespace toolchains {
+
+class LLVM_LIBRARY_VISIBILITY Assigner final : public ToolChain {
+public:
+  Assigner(const Driver &D, const llvm::Triple &Triple,
+              const llvm::opt::ArgList &Args) : ToolChain(D, Triple, Args) {}
+
+private:
+  bool isPICDefault() const override {
+    return false;
+  }
+  bool isPIEDefault(const llvm::opt::ArgList &Args) const override {
+    return false;
+  }
+  bool isPICDefaultForced() const override {
+    return false;
+  }
+  bool hasBlocksRuntime() const override {
+    return false;
+  }
+  bool SupportsProfiling() const override {
+    return false;
+  }
+  void
+  addClangTargetOptions(const llvm::opt::ArgList &DriverArgs,
+                        llvm::opt::ArgStringList &CC1Args,
+                        Action::OffloadKind DeviceOffloadKind) const override {}
+  void
+  AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
+                            llvm::opt::ArgStringList &CC1Args) const override {
+    if (DriverArgs.hasArg(options::OPT_nostdinc, options::OPT_nostdincxx,
+                          options::OPT_nostdlibinc)) {
+      return;
+    }
+    addExternCSystemInclude(DriverArgs, CC1Args, "/usr/include/zkllvm");
+  }
+  void AddClangCXXStdlibIncludeArgs(
+      const llvm::opt::ArgList &DriverArgs,
+      llvm::opt::ArgStringList &CC1Args) const override {
+    addSystemInclude(DriverArgs, CC1Args, "/usr/include/zkllvm/c++");
+  }
+
+};
+
+} // end namespace toolchains
+} // end namespace driver
+} // end namespace clang
+
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ASSIGNER_H

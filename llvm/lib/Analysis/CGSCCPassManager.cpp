745b80efe6411271a7e4e045fa8ab2b46105abdc
Alexander Evgin <aleasims@gmail.com>
2023-06-29 18:25:08 +0000

    Temporarily disable ModuleToPostOrderCGSCC pass
    Should be fixed in the future.
    #56

--- llvm/lib/Analysis/CGSCCPassManager.cpp
+++ llvm/lib/Analysis/CGSCCPassManager.cpp
@@ -136,6 +136,7 @@ PassManager<LazyCallGraph::SCC, CGSCCAnalysisManager, LazyCallGraph &,
 
 PreservedAnalyses
 ModuleToPostOrderCGSCCPassAdaptor::run(Module &M, ModuleAnalysisManager &AM) {
+  return PreservedAnalyses::all(); // FIXME: temporarily disabled.
   // Setup the CGSCC analysis manager from its proxy.
   CGSCCAnalysisManager &CGAM =
       AM.getResult<CGSCCAnalysisManagerModuleProxy>(M).getManager();

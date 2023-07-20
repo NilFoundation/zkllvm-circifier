27b2740c7b463ccc295c37fa83910ac675feae03
Mikhail Aksenov <maksenov@nil.foundation>
2023-07-20 14:18:43 +0000

    Enable instruction printing in release mode

--- llvm/lib/IR/Value.cpp
+++ llvm/lib/IR/Value.cpp
@@ -439,7 +439,6 @@ void Value::takeName(Value *V) {
     ST->reinsertValue(this);
 }
 
-#ifndef NDEBUG
 std::string Value::getNameOrAsOperand() const {
   if (!getName().empty())
     return std::string(getName());
@@ -449,7 +448,6 @@ std::string Value::getNameOrAsOperand() const {
   printAsOperand(OS, false);
   return OS.str();
 }
-#endif
 
 void Value::assertModuleIsMaterializedImpl() const {
 #ifndef NDEBUG
@@ -818,7 +816,7 @@ bool Value::canBeFreed() const {
   // which is why we need the explicit opt in on a per collector basis.
   if (!F->hasGC())
     return true;
-  
+
   const auto &GCName = F->getGC();
   if (GCName == "statepoint-example") {
     auto *PT = cast<PointerType>(this->getType());

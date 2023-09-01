567315f2c68ff15779f6176bb5f8c90f91ac6b86
Alexander Myltsev <a.myltsev@gmail.com>
2023-09-01 07:54:46 +0000

    Squashed 'llvm/utils/crypto/hash/' content from commit fc63d5392d6d
    git-subtree-dir: llvm/utils/crypto/hash
    git-subtree-split: fc63d5392d6dae05694dcb03b2bb2f5c12d5a16d

---
+++ include/nil/crypto3/hash/poseidon.hpp
@@ -0,0 +1,117 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2020 Ilias Khairullin <ilias@nil.foundation>
+// Copyright (c) 2020 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
+#ifndef CRYPTO3_HASH_POSEIDON_HPP
+#define CRYPTO3_HASH_POSEIDON_HPP
+
+#ifdef __ZKLLVM__
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+#else
+#include <nil/crypto3/hash/detail/poseidon/poseidon_permutation.hpp>
+#include <nil/crypto3/hash/detail/sponge_construction.hpp>
+#include <nil/crypto3/hash/detail/block_stream_processor.hpp>
+#endif
+
+namespace nil {
+    namespace crypto3 {
+        namespace hashes {
+
+#ifdef __ZKLLVM__
+            class poseidon {
+            public:
+                typedef typename algebra::curves::pallas::base_field_type::value_type block_type;
+
+                struct process{
+                    block_type operator()(block_type first_input_block, block_type second_input_block){
+                        return __builtin_assigner_poseidon_pallas_base({0, first_input_block, second_input_block})[2];
+                    }
+                };
+            };
+#else
+            template<typename policy_type>
+            class poseidon_compressor {
+            protected:
+                typedef detail::poseidon_permutation<policy_type> poseidon_permutation;
+
+            public:
+                constexpr static const std::size_t word_bits = policy_type::word_bits;
+                typedef typename policy_type::word_type word_type;
+
+                constexpr static const std::size_t state_bits = policy_type::state_bits;
+                constexpr static const std::size_t state_words = policy_type::state_words;
+                typedef typename policy_type::state_type state_type;
+
+                constexpr static const std::size_t block_bits = policy_type::block_bits;
+                constexpr static const std::size_t block_words = policy_type::block_words;
+                typedef typename policy_type::block_type block_type;
+
+                static void process_block(state_type &state, const block_type &block) {
+
+                    for (std::size_t i = 0; i < state_words; ++i)
+                        state[i] ^= block[i];
+
+                    // for (std::size_t i = 0; i != state_words; ++i)
+                    //     boost::endian::endian_reverse_inplace(state[i]);
+
+                    poseidon_permutation::permute(state);
+
+                    // for (std::size_t i = 0; i != state_words; ++i)
+                    //     boost::endian::endian_reverse_inplace(state[i]);
+                }
+            };
+
+            template<typename policy_type>
+            struct poseidon {
+            protected:
+                // typedef policy_type policy_type;
+
+            public:
+                constexpr static const std::size_t word_bits = policy_type::word_bits;
+                typedef typename policy_type::word_type word_type;
+
+                constexpr static const std::size_t block_bits = policy_type::block_bits;
+                constexpr static const std::size_t block_words = policy_type::block_words;
+                typedef typename policy_type::block_type block_type;
+
+                constexpr static const std::size_t digest_bits = policy_type::digest_bits;
+                typedef typename policy_type::digest_type digest_type;
+
+                struct construction {
+                    struct params_type {
+                        typedef typename policy_type::digest_endian digest_endian;
+
+                        // constexpr static const std::size_t length_bits = policy_type::length_bits;
+                        constexpr static const std::size_t digest_bits = policy_type::digest_bits;
+                    };
+
+                    // typedef sponge_construction<params_type, typename policy_type::iv_generator,
+                    //                             poseidon_compressor<FieldType, Rate, Capacity, strength>,
+                    //                             // TODO: padding and finalizer
+                    //                             detail::poseidon_padding<policy_type>,
+                    //                             detail::poseidon_finalizer<policy_type>>
+                    //     type;
+                };
+
+                template<typename StateAccumulator, std::size_t ValueBits>
+                struct stream_processor {
+                    struct params_type {
+                        typedef typename policy_type::digest_endian digest_endian;
+
+                        constexpr static const std::size_t value_bits = ValueBits;
+                    };
+
+                    typedef block_stream_processor<construction, StateAccumulator, params_type> type;
+                };
+            };
+#endif
+        }    // namespace hashes
+    }        // namespace crypto3
+}    // namespace nil
+
+#endif
---
+++ include/nil/crypto3/hash/type_traits.hpp
@@ -0,0 +1,152 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2021 Ilias Khairullin <ilias@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
+
+#ifndef CRYPTO3_HASH_TYPE_TRAITS_HPP
+#define CRYPTO3_HASH_TYPE_TRAITS_HPP
+
+#include <boost/config.hpp>
+
+#ifdef __has_include
+#if __has_include(<version>)
+#include <version>
+#ifdef __cpp_lib_is_constant_evaluated
+#include <type_traits>
+#define CRYPTO3_HAS_IS_CONSTANT_EVALUATED
+#endif
+#endif
+#endif
+
+#ifdef __has_builtin
+#if __has_builtin(__builtin_is_constant_evaluated) && !defined(BOOST_NO_CXX14_CONSTEXPR) && \
+    !defined(BOOST_NO_CXX11_UNIFIED_INITIALIZATION_SYNTAX)
+#define CRYPTO3_HAS_BUILTIN_IS_CONSTANT_EVALUATED
+#endif
+#endif
+//
+// MSVC also supports __builtin_is_constant_evaluated if it's recent enough:
+//
+#if defined(_MSC_FULL_VER) && (_MSC_FULL_VER >= 192528326)
+#define CRYPTO3_HAS_BUILTIN_IS_CONSTANT_EVALUATED
+#endif
+//
+// As does GCC-9:
+//
+#if defined(BOOST_GCC) && !defined(BOOST_NO_CXX14_CONSTEXPR) && (__GNUC__ >= 9) && \
+    !defined(CRYPTO3_HAS_BUILTIN_IS_CONSTANT_EVALUATED)
+#define CRYPTO3_HAS_BUILTIN_IS_CONSTANT_EVALUATED
+#endif
+
+#if defined(CRYPTO3_HAS_IS_CONSTANT_EVALUATED) && !defined(BOOST_NO_CXX14_CONSTEXPR)
+#define CRYPTO3_IS_CONST_EVALUATED(x) std::is_constant_evaluated()
+#elif defined(CRYPTO3_HAS_BUILTIN_IS_CONSTANT_EVALUATED)
+#define CRYPTO3_IS_CONST_EVALUATED(x) __builtin_is_constant_evaluated()
+#elif !defined(BOOST_NO_CXX14_CONSTEXPR) && defined(BOOST_GCC) && (__GNUC__ >= 6)
+#define CRYPTO3_IS_CONST_EVALUATED(x) __builtin_constant_p(x)
+#else
+#define CRYPTO3_NO_CONSTEXPR_DETECTION
+#endif
+
+#define CRYPTO3_CXX14_CONSTEXPR BOOST_CXX14_CONSTEXPR
+//
+// Early compiler versions trip over the constexpr code:
+//
+#if defined(__clang__) && (__clang_major__ < 5)
+#undef CRYPTO3_CXX14_CONSTEXPR
+#define CRYPTO3_CXX14_CONSTEXPR
+#endif
+#if defined(__apple_build_version__) && (__clang_major__ < 9)
+#undef CRYPTO3_CXX14_CONSTEXPR
+#define CRYPTO3_CXX14_CONSTEXPR
+#endif
+#if defined(BOOST_GCC) && (__GNUC__ < 6)
+#undef CRYPTO3_CXX14_CONSTEXPR
+#define CRYPTO3_CXX14_CONSTEXPR
+#endif
+#if defined(BOOST_INTEL)
+#undef CRYPTO3_CXX14_CONSTEXPR
+#define CRYPTO3_CXX14_CONSTEXPR
+#define CRYPTO3_NO_CONSTEXPR_DETECTION
+#endif
+
+#ifdef CRYPTO3_NO_CONSTEXPR_DETECTION
+#define BOOST_CXX14_CONSTEXPR_IF_DETECTION
+#else
+#define BOOST_CXX14_CONSTEXPR_IF_DETECTION constexpr
+#endif
+
+#include <type_traits>
+
+namespace nil {
+    namespace crypto3 {
+        namespace hashes {
+            template<typename Params, typename Hash, typename Group>
+            struct find_group_hash;
+
+            template<typename Params, typename BasePointGeneratorHash, typename Group>
+            struct pedersen_to_point;
+
+            template<typename Params, typename BasePointGeneratorHash, typename Group>
+            struct pedersen;
+
+            template<typename Field, typename Hash, typename Params>
+            struct h2f;
+
+            template<typename Group, typename Hash, typename Params>
+            struct h2c;
+
+            template<typename Hash>
+            struct is_find_group_hash : std::integral_constant<bool, false> { };
+
+            template<typename Params, typename Hash, typename Group>
+            struct is_find_group_hash<find_group_hash<Params, Hash, Group>> : std::integral_constant<bool, true> { };
+
+            template<typename Hash>
+            struct is_pedersen : std::integral_constant<bool, false> { };
+
+            template<typename Params, typename BasePointGeneratorHash, typename Group>
+            struct is_pedersen<pedersen_to_point<Params, BasePointGeneratorHash, Group>>
+                : std::integral_constant<bool, true> { };
+
+            template<typename Params, typename BasePointGeneratorHash, typename Group>
+            struct is_pedersen<pedersen<Params, BasePointGeneratorHash, Group>> : std::integral_constant<bool, true> {
+            };
+
+            template<typename Hash>
+            struct is_h2f : std::integral_constant<bool, false> { };
+
+            template<typename Field, typename Hash, typename Params>
+            struct is_h2f<h2f<Field, Hash, Params>> : std::integral_constant<bool, true> { };
+
+            template<typename Hash>
+            struct is_h2c : std::integral_constant<bool, false> { };
+
+            template<typename Group, typename Hash, typename Params>
+            struct is_h2c<h2c<Group, Hash, Params>> : std::integral_constant<bool, true> { };
+
+        }    // namespace hashes
+    }        // namespace crypto3
+}    // namespace nil
+
+#endif    // CRYPTO3_HASH_TYPE_TRAITS_HPP

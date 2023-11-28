920a6e5d520463205c59b90fca8d0e87f4cf7f9f
Mikhail Sherstennikov <mike.sherstennikov@gmail.com>
2023-11-28 15:04:12 +0200

    Introduce `TupleList` class that implements simple linked list via tvm tuple
    Signed-off-by: Mikhail Sherstennikov <mike.sherstennikov@gmail.com>

---
+++ llvm/projects/ton-compiler/cpp-sdk/tvm/tuple_list.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <tvm/cell.hpp>
+
+namespace tvm {
+
+class TupleList {
+public:
+  __always_inline TupleList() : tuple_(__builtin_tvm_nil()) {}
+
+  __always_inline void push(unsigned v) {
+    tuple_ = __builtin_tvm_tpush(tuple_, v);
+  }
+
+  __always_inline void push(cell v) {
+    tuple_ = __builtin_tvm_tpush(tuple_, v.get());
+  }
+
+  __always_inline void push(TupleList v) {
+    tuple_ = __builtin_tvm_tpush(tuple_, v.tuple());
+  }
+
+  template <unsigned bits> __always_inline void push(uint_t<bits> v) {
+    tuple_ = __builtin_tvm_tpush(tuple_, v.get());
+  }
+
+  template <unsigned bits> __always_inline void push(int_t<bits> v) {
+    tuple_ = __builtin_tvm_tpush(tuple_, v.get());
+  }
+
+  __always_inline __tvm_tuple tuple() { return tuple_; }
+
+private:
+  __tvm_tuple tuple_;
+};
+
+} // namespace tvm

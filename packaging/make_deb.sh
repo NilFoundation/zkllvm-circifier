21f6bb53085413668b8846cea6d7aa7175909777
Mikhail Sherstennikov <mike.sherstennikov@gmail.com>
2023-12-11 14:23:48 +0200

    Squashed 'llvm/projects/evm-sdk/cpp-stdlib/' changes from bc6954b6191e..2b7926f575c1
    2b7926f575c1 Add `libcpp` folder to cmake
    d8f702c228ff Support install-distribution for headers targets
    2113dee44719 Revert "Install zkllvm-libc.ll and related headers by default"
    2feae82d173e Merge pull request #8 from NilFoundation/310_libc_functions
    95d488f18c0b Link strcmp/strncmp, strcpy/strncpy, add zk implementation of strlen
    d26ebc66f0bb Install zkllvm-libc.ll and related headers by default
    37cc730ad224 Add stdlib to deb package
    65fee4fadba0 Initial building of stdlib
    REVERT: bc6954b6191e Draft: initial building of stdlib
    
    git-subtree-dir: llvm/projects/evm-sdk/cpp-stdlib
    git-subtree-split: 2b7926f575c1f4f1c412924eb503880761eb4d27

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -e
 
 if [[ $# != 1 ]]; then
-    echo "Usage: $0 output_dir"
+    echo "Usage: $0 binary_dir"
     exit 1
 fi
-OUTPUT_DIR=$1
+BINARY_DIR=$1
 ROOT_DIR=`dirname -- "$0"`/../
 
 PACKAGE_VERSION=`grep -Po 'Version: \K.*' $ROOT_DIR/packaging/control`
 PACKAGE_NAME="zkllvm-stdlib_${PACKAGE_VERSION}_all"
-INSTALL_PATH=/usr/include/zkllvm
-PACKAGE_DIR=$OUTPUT_DIR/$PACKAGE_NAME
-mkdir -p $PACKAGE_DIR/$INSTALL_PATH/c++
+INCLUDE_PATH=/usr/include/zkllvm
+LIB_PATH=/usr/lib/zkllvm
+PACKAGE_DIR=$BINARY_DIR/$PACKAGE_NAME
+mkdir -p $PACKAGE_DIR/$INCLUDE_PATH/c++
+mkdir -p $PACKAGE_DIR/$LIB_PATH
 mkdir -p $PACKAGE_DIR/DEBIAN
 
 cp $ROOT_DIR/packaging/control $PACKAGE_DIR/DEBIAN
-cp -R $ROOT_DIR/libc/include/* $PACKAGE_DIR/$INSTALL_PATH/
-cp -R $ROOT_DIR/libcpp/* $PACKAGE_DIR/$INSTALL_PATH/c++/
+cp -R $ROOT_DIR/libc/include/* $PACKAGE_DIR/$INCLUDE_PATH/
+cp -R $ROOT_DIR/libcpp/* $PACKAGE_DIR/$INCLUDE_PATH/c++/
+cp $BINARY_DIR/libs/stdlib/libc/zkllvm-libc.ll $PACKAGE_DIR/$LIB_PATH/
 
 dpkg-deb --root-owner-group --build $PACKAGE_DIR

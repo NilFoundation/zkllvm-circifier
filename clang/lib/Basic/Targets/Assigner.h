8329002b32d007ac14a76c5dead0865505af8ac5
Mikhail Aksenov <maksenov@nil.foundation>
2023-12-26 14:39:44 +0000

    Avoid alignment for bls12381 vectors of 2-4 length

--- clang/lib/Basic/Targets/Assigner.h
+++ clang/lib/Basic/Targets/Assigner.h
@@ -29,10 +29,12 @@ public:
       : TargetInfo(Triple) {
     LongWidth = LongAlign = PointerWidth = PointerAlign = 64;
     RegParmMax = 255;
-    // copied from x64_86:
-    resetDataLayout("e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128");
+    // based on x64_86:
+    resetDataLayout("e-m:e-p270:32:32-p271:32:32-p272:64:64-v768:8-v1152:8-"
+                    "v1536:8-i64:64-f80:128-n8:16:32:64-S128");
     MaxAtomicPromoteWidth = 64;
     MaxAtomicInlineWidth = 64;
+    MaxVectorAlign = 8;
   }
 
   void getTargetDefines(const LangOptions &Opts,

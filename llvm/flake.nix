ad0f08f01ad6ad29fdb0d2d7378a3fb56492bc28
Alexander Myltsev <a.myltsev@gmail.com>
2024-01-29 12:24:59 +0000

    Add flake.nix.

---
+++ llvm/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "zkLLVM compiler";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux = {
+      zkllvm = nixpkgs.legacyPackages.x86_64-linux.llvmPackages.stdenv.mkDerivation {
+        pname = "zkllvm";
+        version = "1.0";
+
+        src = self;
+
+        nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [ cmake ninja clang llvm ];
+
+        buildInputs = []; # Add your dependencies here
+
+        cmakeFlags = [
+          "-DCMAKE_C_COMPILER=clang"
+          "-DCMAKE_CXX_COMPILER=clang++"
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DLLVM_ENABLE_PROJECTS=clang"
+          "-DLLVM_TARGETS_TO_BUILD=EVM"
+          "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=EVM"
+          "-DLLVM_CCACHE_BUILD=ON"
+          "-DLLVM_ENABLE_LLD=ON"
+          "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
+          "-DLLVM_INSTALL_TOOLCHAIN_ONLY=ON"
+          "-DDEFAULT_SYSROOT=/usr"
+          "-DEVM_HOST=ON"
+          "-DLLVM_DISTRIBUTION_COMPONENTS='clang;evm-stdlib;evm-linker;zkllvm-cxx-headers;zkllvm-c-headers'"
+        ];
+      };
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.zkllvm;
+  };
+}
